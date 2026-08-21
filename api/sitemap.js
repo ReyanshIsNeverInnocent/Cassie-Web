96c7ab145036a2821855ea4a5b619fceb91ba5ef
ReyuFellOff <relish.reyansh.489@outlook.com>
2026-08-22 02:39:16 +0530

    ON A LONELY NIGHT!

---
+++ api/sitemap.js
@@ -0,0 +1,27 @@
+const routes = ['/', '/commands', '/about', '/stats', '/privacy', '/terms'];
+
+function escapeXml(value) {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+}
+
+export default function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
+    return res.status(405).end();
+  }
+
+  const baseUrl = 'https://levitate-web.vercel.app';
+  const urls = routes
+    .map((route) => `    <url><loc>${escapeXml(`${baseUrl}${route}`)}</loc></url>`)
+    .join('\n');
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`;
+
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.setHeader('Cache-Control', 'public, s-maxage=3600, stale-while-revalidate=86400');
+  return res.status(200).send(sitemap);
+}
